af752bea2b40e9a0205d045114c2f6a3265b3c62
wibastidas <wibastidas77@gmail.com>
2018-11-04 17:55:25 -0300

    config service worker and add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import {MatButtonModule, MatToolbarModule} from '@angular/material';
+import { MatExpansionModule } from '@angular/material';
+
+import { MatFormFieldModule } from '@angular/material';
+
+import { MatInputModule } from '@angular/material';
+
 
 @NgModule({
   declarations: [
@@ -13,7 +21,13 @@ import { environment } from '../environments/environment';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
